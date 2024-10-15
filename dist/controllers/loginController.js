5acef804bb22cc5cc4ef375ba8947974ab4ccb64
shaileshtiwari21 <saffron.shaileshtiwari@gmail.com>
2024-10-15 17:33:13 +0530

    Chnages

---
+++ dist/controllers/loginController.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginController = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+// src/controllers/loginController.ts
+const User_1 = __importDefault(require("../schemas/User"));
+// console.log(process.env.JWT_SECRET);
+const loginController = async (req, res) => {
+    try {
+        // Input validation
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ message: "Email and password are required" });
+        }
+        // Find user by email
+        const user = await User_1.default.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        // Check password
+        const passwordMatch = await bcryptjs_1.default.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+        // Password is correct, generate JWT token
+        const token = jsonwebtoken_1.default.sign({ userId: user._id }, process.env.JWT_SECRET);
+        // Return JWT token in response
+        res.status(200).json({ message: "Login successful", token });
+    }
+    catch (error) {
+        console.error("Error logging in:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+exports.loginController = loginController;
+//# sourceMappingURL=loginController.js.map
