7ad27b36bd5f36d97641083c765af65ee77864e0
Abhaya <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2024-11-26 10:13:37 +0530

    bug fixed

--- dist/schemas/User.js
+++ dist/schemas/User.js
@@ -8,12 +8,12 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const userSchema = new mongoose_1.default.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: String, required: false },
     age: { type: Number, required: true },
     height: { type: Number, required: true },
     weight: { type: Number, required: true },
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    lastName: { type: String, required: false },
     gender: { type: String, required: true },
     otp: {
         type: String,
